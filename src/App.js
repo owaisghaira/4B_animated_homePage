ae6ee85cfee33b8adfae4eced7fcac6b94f13c49
owaisghaira <owais.ghaira99@gmail.com>
2021-06-22 22:50:36 +0500

    home page created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import NavBar from './components/NavBar'
+import { Grid, Typography,Button } from '@material-ui/core'
+import Intro from './Images/Intro_Feature.svg'
+import IntroBrain from './Images/Intro_Brain.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <NavBar />
+      <Grid style={{ padding: 30 }} justify='center' container>
+        <Grid item xs={12} md={6}>
+          <h1 className='heading'> CORTEX </h1>
+          <h5 className='heading2'>COPYWRITER</h5>
+          <h5 style={{ color: 'white' }}>Enhance your communications with psychology-based copywriting and UX writing</h5>
+          <Button size="large">
+          Large
+        </Button>
+        </Grid>
+        <Grid className='intro' item xs={12} md={6}>
+          <img src={IntroBrain} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
